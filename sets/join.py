743debc24638f792806823d0ad5553d4f3385cc7
DESKTOP-DHHTU1I\natch <natchaya.no@ku.th>
2021-01-17 11:49:30 +0700

    add

--- sets/join.py
+++ sets/join.py
@@ -1,8 +1,7 @@
 set1 = {"a", "b", "c"}
 set2 = {"d", "e", "f"}
 set3 = set1.union(set2)
-print(set3)
-
+print(set3)  # {'c', 'a', 'd', 'e', 'b', 'f'}
 
 set1 = {"a", "b", "c"}
 set2 = {"d", "e", "f"}
@@ -13,10 +12,10 @@ print(set1)
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "micrisoft", "apple"}
 set1.intersection_update(set2)
-print(set1)
+print(set1)  # เอาสิ่งที่ซ้ำออกมา
 
 
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "micrisoft", "apple"}
 set1.symmetric_difference_update(set2)
-print(set1)
+print(set1)  # สิ่งท่เหมือนกันถูฏตัดออกไป
