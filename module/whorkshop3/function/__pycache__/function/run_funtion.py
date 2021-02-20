a08abfeb56c02e7781529372f9def2229645e8e8
DESKTOP-DHHTU1I\natch <natchaya.no@ku.th>
2021-02-20 15:08:08 +0700

    add

---
+++ module/whorkshop3/function/__pycache__/function/run_funtion.py
@@ -0,0 +1,10 @@
+import calculate
+
+
+number1 = 10
+number2 = 2
+
+print(calculate.add(number1, number2))
+print(calculate.subtract(number1, number2))
+print(calculate.multiplied(number1, number2))
+print(calculate.divide(number1, number2))
