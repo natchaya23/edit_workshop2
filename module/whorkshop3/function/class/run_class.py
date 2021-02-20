7af40c7276e2f3f4d38ae2cb0a2c3b5662318c86
DESKTOP-DHHTU1I\natch <natchaya.no@ku.th>
2021-02-20 15:13:14 +0700

    add

---
+++ module/whorkshop3/function/class/run_class.py
@@ -0,0 +1,12 @@
+import classPerson
+
+name = "pipusana petgumpoom"
+age = 21
+address = "London"
+
+person = classPerson.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_info())
---
+++ module/whorkshop3/function/variable/variables.py
@@ -0,0 +1,2 @@
+person = {"name": "nam", "age": 21, "country": "London"}
+person = {"name": "aum", "age": 20, "country": "London"}
